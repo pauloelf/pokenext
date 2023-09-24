7e1cd0ca34aa812ecdc625546aa5be0320004d76
Paulo Sérgio <97705403+pauloelf@users.noreply.github.com>
2023-09-23 22:48:28 -0300

    feat: adicionado lista de cards de pokemons na home
    Consumindo a api para mostrar os pokemons em cards

---
+++ src/components/Card.tsx
@@ -0,0 +1,22 @@
+import { Pokemon } from "@/app/page";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Card({pokemon}: {pokemon: Pokemon}) {
+  const response = await fetch(pokemon.url)
+  const details = await response.json()
+
+  return (
+    <li className='card'>
+      <Image
+        src={details.sprites.front_default}
+        width='120'
+        height='120'
+        alt={pokemon.name}
+      />
+      <p className='flex justify-center items-center bg-[#e33d33] text-white p-1 my-4 rounded-md'>#{details.id}</p>
+      <h3 className='text-2xl capitalize mb-4'>{pokemon.name}</h3>
+      <Link className='card-button' href={`/pokemon/${pokemon.id}`}>Details</Link>
+    </li>
+  )
+}
